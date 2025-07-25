4cb5c123282a097b35570a5cc4bfc3ae14d1b876
SAGAR CHAUHAN <151016944+sagarrrr21@users.noreply.github.com>
2025-07-25 02:42:53 +0000

    Route commit

---
+++ mystrymessage/src/app/api/signup/route.ts
@@ -0,0 +1,101 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import dbConnect from "@/lib/dbConnet";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { date, success } from "zod";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, email, password } = await request.json();
+    const existingUSerVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUSerVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exist with this email",
+          },
+          { status: 400 }
+        );
+      } else {
+        const HashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = HashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
+      }
+    } else {
+      const HashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: HashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+      await newUser.save();
+    }
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User resgistered successfully. Please verify your email",
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error registering user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
--- mystrymessage/src/helpers/sendVerificationEmail.ts
+++ mystrymessage/src/helpers/sendVerificationEmail.ts
@@ -6,16 +6,20 @@ import { ApiResponse } from "@/types/ApiResponse";
 import { string, success } from "zod";
 
 export async function sendVerificationEmail(
-    email: string,
-    username: string,
-    verifyCode: string
+  email: string,
+  username: string,
+  verifyCode: string
 ): Promise<ApiResponse> {
-    try {
-        return { success: true, message: "verification email send successfully" }
-
-    } catch (emailError) {
-        console.error("Error sending verification email ", emailError)
-        return { success: false, message: "Failed to send verification email" }
-    }
-
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Mystry message | Verification code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: "verification email send successfully" };
+  } catch (emailError) {
+    console.error("Error sending verification email ", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
 }
